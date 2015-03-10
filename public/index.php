c558011945d7f7800ce5edb26f1e951af869557d
root <root@s230>
2015-03-10 13:30:00 +0800

    msg

--- public/index.php
+++ public/index.php
@@ -3,8 +3,11 @@
  * 启动器
  */
 date_default_timezone_set ( 'Asia/Shanghai' );
-if(time()>strtotime('2014-02-11 19:00:00') && time() < strtotime('2014-02-11 22:00:00')) {
-	$html = <<<EFO
+define ( 'MSD_FORCE_CITY', getenv ( 'MSD_FORCE_CITY' ) ? getenv ( 'MSD_FORCE_CITY' ) : 'wuxi' );
+
+$img_url = MSD_FORCE_CITY == 'nanjing'? '/common/images/njclose.jpg':'/common/images/jiaqi.jpg';
+
+$html = <<<EFO
 	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 	<html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
@@ -13,13 +16,20 @@ if(time()>strtotime('2014-02-11 19:00:00') && time() < strtotime('2014-02-11 22:
 	</head>
 	
 	<body style="margin:0px auto;width:960px">
-	<img src="/common/images/1.jpg" />
+	<img src="{$img_url}" />
 	</body>
 	</html>
 EFO;
+
+//if(time()>strtotime('2015-02-15 14:00:00') && time<strtotime('2015-02-26 08:00:00')){
+//	echo $html;
+//	exit;
+//}
+
+if(time()>strtotime('2015-01-15 21:00:00') && MSD_FORCE_CITY == 'nanjing') {
 	echo $html;
 	exit;
-} 
+}
 define ( 'FANDIAN_APP_VER', '4c95932f40b9ca06' );
 /*
 if (getenv ( 'FANDIAN_APP_VER' )) {
@@ -50,7 +60,6 @@ define ( 'MSD_ONE_DAY', 86400 );
 define ( 'MSD_START_TIME', microtime () );
 define ( 'MSD_FORCE_RELOAD_CONFIG', true );
 define ( 'MSD_LOCAL_CACHER', getenv ( 'MSD_LOCAL_CACHER' ) ? getenv ( 'MSD_LOCAL_CACHER' ) : 'apc' );
-define ( 'MSD_FORCE_CITY', getenv ( 'MSD_FORCE_CITY' ) ? getenv ( 'MSD_FORCE_CITY' ) : 'wuxi' );
 
 defined ( 'APPLICATION_PATH' ) || define ( 'APPLICATION_PATH', realpath ( dirname ( __FILE__ ) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'application' ) );
 defined ( 'APPLICATION_ENV' ) || define ( 'APPLICATION_ENV', (getenv ( 'APPLICATION_ENV' ) ? getenv ( 'APPLICATION_ENV' ) : 'development') );
